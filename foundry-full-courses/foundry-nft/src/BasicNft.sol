5ce8acff119852f20853810da3b186357892269c
NextEcho <nextecho@outlook.com>
2024-05-03 17:01:20 +0800

    feat(foundry-nft): 完成 NFT 基础部署脚本和测试

--- foundry-full-courses/foundry-nft/src/BasicNft.sol
+++ foundry-full-courses/foundry-nft/src/BasicNft.sol
@@ -4,17 +4,23 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    // NFT 代币是代币ID和地址的组合
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
     // EIP721 中最重要的函数之一: tokenURI
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return "saldkjsakld";
+        return s_tokenIdToUri[tokenId];
     }
 }

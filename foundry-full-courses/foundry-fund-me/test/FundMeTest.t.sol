9d9c2b522402257014e790e0f4e2e030f96b413f
NextEcho <nextecho@outlook.com>
2024-05-29 11:24:24 +0800

    feat(foundry-fund-me): 基础测试

---
+++ foundry-full-courses/foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function testDemo() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.getOwner(), address(this));
+    }
+}

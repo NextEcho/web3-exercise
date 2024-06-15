68b026d658c6103049d52cdd4c4628c0b6f922f7
NextEcho <nextecho@outlook.com>
2024-06-15 08:50:18 +0800

    feat: mint nft success

--- foundry-full-courses/foundry-nft/script/Interactions.s.sol
+++ foundry-full-courses/foundry-nft/script/Interactions.s.sol
@@ -4,14 +4,18 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import "forge-std/StdJson.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
-        address mostRecentlyDeployContract = DevOpsTools
-            .get_most_recent_deployment("BasicNft", block.chainid);
+        // address mostRecentlyDeployContract = DevOpsTools
+        //     .get_most_recent_deployment("BasicNft", block.chainid);
+
+        address mostRecentlyDeployContract = getDeployedContractAddress();
 
         mintNftOnContract(mostRecentlyDeployContract);
     }
@@ -20,5 +24,29 @@ contract MintBasicNft is Script {
         vm.startBroadcast();
         BasicNft(contractAddress).mintNft(PUG);
         vm.stopBroadcast();
+    }
+
+    // Tool Function
+    function getDeployedContractAddress() private view returns (address) {
+        string memory path = string.concat(
+            vm.projectRoot(),
+            "/broadcast/DeployBasicNft.s.sol/",
+            Strings.toString(block.chainid),
+            "/run-latest.json"
+        );
+        string memory json = vm.readFile(path);
+        bytes memory contractAddress = stdJson.parseRaw(
+            json,
+            ".transactions[0].contractAddress"
+        );
+        return (bytesToAddress(contractAddress));
+    }
+
+    function bytesToAddress(
+        bytes memory bys
+    ) private pure returns (address addr) {
+        assembly {
+            addr := mload(add(bys, 32))
+        }
     }
 }
